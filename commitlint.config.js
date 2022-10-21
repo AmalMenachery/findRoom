b1346389de86fb5375fe49c5132b7abb891418e7
Amal <menacheryamal@gmail.com>
2022-10-22 00:45:16 +0200

    feat: initial commit

---
+++ commitlint.config.js
@@ -0,0 +1,32 @@
+module.exports = { extends: ['@commitlint/config-conventional'] };
+// module.exports = {
+//     parserPreset: [
+//       'conventional-changelog-conventionalcommits',
+//       {
+//         parseOpts: {
+//           issuePrefixes: 'JIRA-'
+//         }
+//       }
+//     ],
+//     rules: {
+//       'body-leading-blank': [1, 'always'],
+//       'body-empty': [2, 'never'],
+//       'body-max-line-length': [2, 'always', 100],
+//       'footer-leading-blank': [1, 'always'],
+//       'footer-max-line-length': [2, 'always', 100],
+//       'header-max-length': [2, 'always', 100],
+//       'scope-case': [2, 'always', 'lower-case'],
+//       'scope-empty': [2, 'never'],
+//       'scope-max-length': [2, 'always', 10],
+//       'scope-enum': [2, 'always', ['lpg', 'i&c', 'lubes', 'retail']],
+//       'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+//       'subject-empty': [2, 'never'],
+//       'subject-full-stop': [2, 'never', '.'],
+//       'type-case': [2, 'always', 'lower-case'],
+//       'type-empty': [2, 'never'],
+//       'type-enum': [2, 'always', ['build', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
+//       'references-empty': [1, 'never']
+//     },
+//     helpUrl:
+//     'https://github.com/conventional-changelog/commitlint/#what-is-commitlint'
+//   };
